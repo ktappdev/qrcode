eeaae6770767b6297bc4d25b38864ece22ee07be
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-05-18 16:51:46 -0400

    link exchanger working, little finiky to test but seems solid

--- main.go
+++ main.go
@@ -1,43 +1,60 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 	"sync"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/ktappdev/qrcode/qrcode"
+	"github.com/ktappdev/qrcode/urlexchanger"
 	"golang.org/x/time/rate"
 )
 
 var limiter = NewIPRateLimiter(1)
+var exchanger = urlexchanger.NewURLExchanger()
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8081"
+	}
+
 	router := gin.Default()
 
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"*", "http://localhost:3000", "http://localhost:8081", "http://192.168.1.14"}
 	config.AllowMethods = []string{"GET"}
 	router.Use(cors.New(config))
-	router.GET("/qrcode", func(c *gin.Context) {
-		clientIP := c.ClientIP()
-		if !limiter.Allow(clientIP) {
-			c.String(http.StatusOK, "Slowdown cowboy!, %v request per second", limiter.rateLimit)
-			return
-		}
-		data := "https://www.lugetech.com"
-		size := -10 // -10 will make each qr pixel 10x10, i can do 256 which would give 256x256px image but there is usually white space around it
+	router.GET("/qrcode", getQr)
+	router.GET("/qr", exchanger.HandleQRCodeInteraction)
+	router.Run(port)
+}
 
-		qrCodeBytes, err := qrcode.GenerateQRCode(data, size)
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Error generating QR code")
-			return
-		}
+func getQr(c *gin.Context) {
+	clientIP := c.ClientIP()
+	if !limiter.Allow(clientIP) {
+		c.String(http.StatusOK, "Slowdown cowboy!, %v request per second", limiter.rateLimit)
+		return
+	}
+	// data := "https://www.lugetech.com"
+	size := -10 // -10 will make each qr pixel 10x10, i can do 256 which would give 256x256px image but there is usually white space around it
+	originalURL := "https://www.lugetech.com"
+	qrCodeURL := exchanger.GenerateQRCodeURL(originalURL)
+	qrCodeBytes, err := qrcode.GenerateQRCode(qrCodeURL, size)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Error generating QR code")
+		return
+	}
 
-		c.Data(http.StatusOK, "image/png", qrCodeBytes)
-	})
-
-	router.Run(":8081")
+	c.Data(http.StatusOK, "image/png", qrCodeBytes)
 }
 
 type IPRateLimiter struct {
