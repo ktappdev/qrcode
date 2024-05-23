baf20debf043f13519ea69ae91e3894982ec9bca
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-05-23 01:12:05 -0400

    init mango in main - env in action

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/ktappdev/qrcode-server/mongodb"
 	"github.com/ktappdev/qrcode-server/ratelimiter"
 	"github.com/ktappdev/qrcode-server/urlexchanger"
 	_ "image/jpeg"
@@ -23,16 +24,28 @@ func init() {
 	image.RegisterFormat("bmp", "bmp", bmp.Decode, bmp.DecodeConfig)
 }
 
+func initMongoDB(MONGO_URL string) {
+	err := mongodb.Connect(MONGO_URL)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB Atlas: %v", err)
+	}
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
+	}
+	MONGO_URL := os.Getenv("MONGO_URL")
+	if MONGO_URL == "" {
+		log.Fatal("MONGO_URL environment variable is not set")
 	}
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = ":8081"
 	}
 
+	initMongoDB(MONGO_URL)
 	router := gin.Default()
 	// Set the maximum memory limit for parsing multipart forms
 	router.MaxMultipartMemory = 10 << 20 // 10MB
