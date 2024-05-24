a149cdfe6df131a22593d6f2a1d95a14026dfa01
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-05-24 00:05:58 -0400

    added geo-lite for ip lookup

--- urlexchanger/urlexchanger.go
+++ urlexchanger/urlexchanger.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"os"
 	"sync"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -20,33 +19,33 @@ func getEnvItems() (port string, server string) {
 }
 
 type URLExchanger struct {
-	mu         sync.RWMutex
-	qrCodeURLs map[string]string
+	mu            sync.RWMutex
+	qrCodeURLsMap map[string]string
 }
 
 func NewURLExchanger() *URLExchanger {
 	return &URLExchanger{
-		qrCodeURLs: make(map[string]string),
+		qrCodeURLsMap: make(map[string]string),
 	}
 }
 
-func (e *URLExchanger) GenerateQRCodeURL(originalURL string, backgroundColour, qrCodeColour string, name string) string {
+func (e *URLExchanger) GenerateQRCodeURL(originalLink string, backgroundColour, qrCodeColour string, name string) string {
 	port, server := getEnvItems()
 	uniqueID := uuid.New().String()
 
 	//NOTE: Store the mapping in the Map (Keeping this for speed)
 	e.mu.Lock()
-	e.qrCodeURLs[uniqueID] = originalURL
+	e.qrCodeURLsMap[uniqueID] = originalLink
 	e.mu.Unlock()
 
 	//NOTE: Store the mapping in the database
-	err := mongodb.InsertQRCodeURL(uniqueID, originalURL, backgroundColour, qrCodeColour, name)
+	err := mongodb.InsertQRCodeURL(uniqueID, originalLink, backgroundColour, qrCodeColour, name)
 	if err != nil {
 		log.Println("Error inserting URL into database")
 		log.Fatal(err)
 	}
 
-	fmt.Println("list of QR Codes:", e.qrCodeURLs)
+	log.Println("list of QR Codes:", e.qrCodeURLsMap)
 	var link string
 	if server != "https://qr.lugetech.com" {
 		fmt.Println("Using local server with port, if this is running on the remote server it will not work")
@@ -61,28 +60,22 @@ func (e *URLExchanger) GenerateQRCodeURL(originalURL string, backgroundColour, q
 
 func (e *URLExchanger) HandleQRCodeInteraction(c *gin.Context) {
 	uniqueID := c.Query("id")
-	start := time.Now() // Record the start time
 	e.mu.RLock()
-	originalURL, exists := e.qrCodeURLs[uniqueID]
+	originalURL, exists := e.qrCodeURLsMap[uniqueID]
 	e.mu.RUnlock()
-	elapsed := time.Since(start) // Calculate the elapsed time
 	if exists {
 		//NOTE: Mapping found in the in-memory map
 		mongodb.LogQRCodeInteraction(uniqueID, c.Request)
-		log.Printf("Link returned from in-memory: %s (Took %v)", originalURL, elapsed)
 		log.Println("this is the link returned from in-memory", originalURL)
 		c.Redirect(http.StatusFound, originalURL)
 		return
 	}
 
 	//NOTE: Mapping not found in the in-memory map, check the database
-	start = time.Now() // Record the start time for MongoDB operation
 	originalURL, err := mongodb.GetQRCodeURL(uniqueID)
-	elapsed = time.Since(start) // Calculate the elapsed time
 	if err == nil {
 		// Mapping found in the database
 		mongodb.LogQRCodeInteraction(uniqueID, c.Request)
-		log.Printf("Link returned from MongoDB: %s (Took %v)", originalURL, elapsed)
 		log.Println("this is the link returned from mongodb", originalURL)
 		c.Redirect(http.StatusFound, originalURL)
 		return
@@ -92,12 +85,3 @@ func (e *URLExchanger) HandleQRCodeInteraction(c *gin.Context) {
 	fmt.Printf("%s NOT FOUND IN MEMORY OR DATABASE", uniqueID)
 	c.Status(http.StatusNotFound)
 }
-
-// func (e *URLExchanger) logInteraction(uniqueID string, r *http.Request) {
-// 	timestamp := time.Now().Format(time.RFC3339)
-// 	userAgent := r.UserAgent()
-// 	ipAddress := r.RemoteAddr
-// 	referer := r.Referer()
-// 	fmt.Printf("QR Code Interaction - ID: %s, Timestamp: %s, User Agent: %s, IP Address: %s, referrer: %s", uniqueID, timestamp, userAgent, ipAddress, referer)
-//
-// }
